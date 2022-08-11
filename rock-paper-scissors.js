9a08308592ae46796c8ea7bbd61997ecf06d8cc8
Davy Sproule <dsproule@vt.edu>
2022-08-11 07:07:47 -0400

    Iniitialization and completion of getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,28 @@
+function getComputerChoice() {
+    // Create array of Rock, Paper, Scissors
+    const choices = ["rock", "paper", "scissors"]
+    // Randomly index one of these
+    computerSelection = choices[Math.floor(Math.random()*3)]
+    return computerSelection
+}
+
+function playRound (playerSelection, computerSelection){
+    // Paper beats Rock player wins
+    if (playerSelection === "paper")
+    // Paper beats Rock computer wins
+
+    // Rock beats Scissors player wins
+
+    // Rock beats Schissors computer wins
+
+    // Scissors beats Paper player wins
+
+    // Scissors beats Paper computer wins
+
+    // Tie
+}
+
+function game(){
+    // play 5 rounds and keep score
+}
+
