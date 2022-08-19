84953698ccd8c85b98aab6e4589324d176076867
Davy Sproule <dsproule@vt.edu>
2022-08-19 14:51:07 -0400

    Update of game logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -37,7 +37,7 @@ function playRound (playerSelection, computerSelection){
     }
     // Scissors beats Paper computer wins
     else if (playerSelection === "paper" && computerSelection === "scissors"){
-        msg = "You Lose! Paper beats Rock";
+        msg = "You Lose! Scissors beats Paper";
         winner = "computer";
     }
     // Tie
@@ -50,46 +50,110 @@ function playRound (playerSelection, computerSelection){
 
 }
 
-function game(){
-    // play 5 rounds and keep score
-    console.log("Let\'s play 5 rounds of rock paper scissors!");
+//function game(){
+//     // play 5 rounds and keep score
+//     console.log("Let\'s play 5 rounds of rock paper scissors!");
     
-    // Initialize Scores
-    let playerScore = 0;
-    let computerScore = 0;
-    let tie = 0;
+//     // Initialize Scores
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let tie = 0;
 
-    for (let i = 0; i < 5; i++){
-        // Prompt for each round
-        let validEntry = false;
 
-        while (!validEntry){
-            playerSelection = (prompt("Play Rock, Paper, or Scissors")).toLowerCase();
-            if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){
-                validEntry = true;
-            }
-            else{
-                console.log('Invalid entry, try again')
-            }
-        }
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection,computerSelection);
+
+//     for (let i = 0; i < 5; i++){
+//         // Prompt for each round
+//         let validEntry = false;
+
+//         while (!validEntry){
+//             playerSelection = (prompt("Play Rock, Paper, or Scissors")).toLowerCase();
+//             if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){
+//                 validEntry = true;
+//             }
+//             else{
+//                 console.log('Invalid entry, try again')
+//             }
+//         }
+//         let computerSelection = getComputerChoice();
+//         let result = playRound(playerSelection,computerSelection);
         
-        // Add to Score Totals
-        if (result.winner === 'player'){
-            playerScore++;
-        }
-        else if (result.winner === 'computer'){
-            computerScore++;
-        }
-        else {
-            tie++;
-        }
+//         // Add to Score Totals
+//         if (result.winner === 'player'){
+//             playerScore++;
+//         }
+//         else if (result.winner === 'computer'){
+//             computerScore++;
+//         }
+//         else {
+//             tie++;
+//         }
 
-        // Write out round result and updated scores
-        console.log(result.message)
-        console.log(`-----SCORE-----\n Player: ${playerScore}\n Computer: ${computerScore}\n Ties: ${tie}\n---------------`);
+//         // Write out round result and updated scores
+//         console.log(result.message)
+//         console.log(`-----SCORE-----\n Player: ${playerScore}\n Computer: ${computerScore}\n Ties: ${tie}\n---------------`);
+//     }
+// }
+
+// Game Logic for single round
+
+//game()
+
+function updateScoreboard(winner){
+    let playerScore = document.querySelector('#player-score');
+    let computerScore = document.querySelector('#computer-score');
+
+    // Add to Score Totals    
+    if (winner === 'player'){
+            playerScore.textContent++;
+        }
+    else if (winner === 'computer'){
+            computerScore.textContent++;
+        }
+    else {}
+}
+
+function checkWinner(){
+    // Get Current Scores
+    let playerScore = document.querySelector('#player-score').textContent;
+    let computerScore = document.querySelector('#computer-score').textContent;
+    
+    // Initialize Variables
+    let clearBoard = false;
+    let winner = '';
+    
+    // Check for Winner
+    if (parseInt(playerScore) === 5){
+        clearBoard = true;
+        winner = 'Player'
+    }
+    else if (parseInt(computerScore) === 5){
+        clearBoard = true
+        winner = 'Computer'
+    }
+
+    // If winner found, declare winner and deactivate buttons
+    if (clearBoard){
+        document.getElementById('button-container').style.display = 'none'
+        const endGame = document.createElement('div');
+        endGame.textContent = `${winner} reached 5 points! They are the winner!`;
+        document.body.appendChild(endGame)
     }
 }
 
-game()
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e)=> {
+        let result = playRound(e.target.id,getComputerChoice());
+        
+        //Update result div
+        const output = document.querySelector('#output');
+        output.textContent = result.message;
+
+        //Update scoreboard
+        updateScoreboard(result.winner)
+
+        //Check for game completion
+        checkWinner()
+    })
+})
